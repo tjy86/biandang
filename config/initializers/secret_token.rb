986f7de5bd5334b3455ecf2273acd7549887e2be
tjy86 <thomasjyang@gmail.com>
2013-02-15 13:59:18 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Biandang::Application.config.secret_token = 'ffbe539b6f6b6b7a4c6d065741524a5d44015a353065bd17bb5fbdcb272e78fdb25b3d5e0fd597b31c62791e193225889180fe5ad43cc9c147fbdb9e7a8d93e6'
